a3f3f5ff181cd7100e1a3f6ade0b0f4540c7ec6f
Keith Merritt <keithtroymerritt@gmail.com>
2021-05-23 15:09:28 -0300

    Front End Start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Pureblnd</h1>
+      <Header />
+        <main>
+        <Container>
+          <h1>Pureblnd</h1>
+        </Container>
+        </main>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>I am a footer</h2>
+        </footer>
+    )
+}
+
+export default Footer
